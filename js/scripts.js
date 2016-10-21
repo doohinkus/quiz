79a9064173d9768fdf6f57cc7d664dd5143b2308
Rafael Perez <doohinkus@gmail.com>
2016-10-21 11:42:41 -0700

    begin business logic for evaluating the form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*----BUSINESS LOGIC----*/
-
+//move from panel to panel. There's gotta be a better way!!!!
 var switchPanel = function (currentPanel, nextPanel){
   //hide current panel
   $(currentPanel).slideUp();
@@ -7,28 +7,37 @@ var switchPanel = function (currentPanel, nextPanel){
   $(nextPanel).slideDown();
 }
 
+var evaluateResults = function () {
+  var q1Response = $("#question1").val();
+  console.log(q1Response);
+
+
+}
+
 
 
 /*----INTERFACE----*/
-
 $(document).ready(function(){
-  //Take Quiz Button Takes User to Quiz
-   $("#takeQuiz").click(function (){
-     //Take user to main content area
-     window.location.href = "#footer";
 
-   });
-   //Evaluate and display quiz results
+  //Evaluate and display quiz results
    $("form#epicodusQuiz").submit(function (event){
+     evaluateResults();
      event.preventDefault();
    });
 
-   //Start Quiz!!!!
+   //Quiz Button Takes User to Quiz Section
+    $("#takeQuiz").click(function (){
+     //Take user to quiz section
+      window.location.href = "#footer";
+    });
+
+  //Start Quiz!!!!
    $("#startQuiz").click(function (){
      switchPanel("#panel1", "#panel2");
 
    });
 
+  //There's gotta be a better way!!!!
    $("#p2prev").click(function(){
     switchPanel($(this).parent().parent(), "#panel1");
    });
@@ -39,6 +48,10 @@ $(document).ready(function(){
 
    $("#p3prev").click(function(){
     switchPanel($(this).parent().parent(), "#panel2");
+   });
+
+   $("#p3next").click(function(){
+    switchPanel($(this).parent().parent(), "#panel4");
    });
 
 

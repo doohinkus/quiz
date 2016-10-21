71513ed180b08db466e29869f8b93097c973cd6c
Rafael Perez <doohinkus@gmail.com>
2016-10-21 09:58:42 -0700

    inelegantly add sliding between panels with quiz questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 /*----BUSINESS LOGIC----*/
 
+var switchPanel = function (currentPanel, nextPanel){
+  //hide current panel
+  $(currentPanel).slideUp();
+  //move to next one
+  $(nextPanel).slideDown();
+}
+
+
+
 /*----INTERFACE----*/
 
 $(document).ready(function(){
@@ -7,11 +16,33 @@ $(document).ready(function(){
    $("#takeQuiz").click(function (){
      //Take user to main content area
      window.location.href = "#mainContent";
-    
-   });
 
-   $("form#triInput").submit(function (event){
+   });
+   //Evaluate and display quiz results
+   $("form#epicodusQuiz").submit(function (event){
      event.preventDefault();
    });
+
+   //Start Quiz!!!!
+   $("#startQuiz").click(function (){
+     switchPanel("#panel1", "#panel2");
+
+   });
+
+   $("#p2prev").click(function(){
+    switchPanel($(this).parent().parent(), "#panel1");
+   });
+
+   $("#p2next").click(function(){
+    switchPanel($(this).parent().parent(), "#panel3");
+   });
+
+   $("#p3prev").click(function(){
+    switchPanel($(this).parent().parent(), "#panel2");
+   });
+
+
+    // $("#myModal").modal('show');
+    //.modal content
 
 });
